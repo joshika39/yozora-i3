8565fcdbf7743e2a1a7ee49fa811c3fd60a26bc2
Joshua Hegedus <josh.hegedus@outlook.com>
2024-05-08 16:27:51 +0200

    progress

--- scripts/tailscale.sh
+++ scripts/tailscale.sh
@@ -4,7 +4,6 @@
 
 # Usage: tailscale.sh [--select-exitnode] [--reset] [--down]
 
-
 # If tailscale is not installed, exit
 
 if ! command -v tailscale &> /dev/null
@@ -16,10 +15,63 @@ fi
 # If script is ran without any arguments, show the main menu
 # Main menu options: Start: if not running; Stop, Restart, Select Exit Node, Reset: if running
 
-# Use Rofi to show the menu
-
-# is_running is false when the output of tailscale status is not "Tailscale is stopped"
 is_running=$(tailscale status | grep -q "Tailscale is stopped"; echo $?)
 
-echo "is_running: $is_running"
+if [ "$is_running" -eq 0 ]; then
+    selected=$(echo -e "Start\nQuit" | rofi -dmenu -p "Tailscale is not running")
+else
+    selected=$(echo -e "Stop\nRestart\nSelect Exit Node\nReset\nQuit" | rofi -dmenu -p "Tailscale is running")
+fi
+
+if [ "$selected" == "Quit" ]; then
+    exit
+fi
+
+if [ "$selected" == "Start" ]; then
+    sudo tailscale up
+    exit
+fi
+
+if [ "$selected" == "Stop" ]; then
+    sudo tailscale down
+    exit
+fi
+
+if [ "$selected" == "Restart" ]; then
+    sudo tailscale down
+    sudo tailscale up
+    exit
+fi
+
+if [ "$selected" == "Select Exit Node" ]; then
+    # Show the list of exit nodes with the "<ip> - <hostname>" format
+    selected=$(tailscale status | grep -v "exit node" | awk '{print $1" - "$2}' | rofi -dmenu -p "Select Exit Node")
+
+    if [ -z "$selected" ]; then
+        exit
+    fi
+
+    selected_ip=$(echo $selected | awk -F' - ' '{print $1}')
+    selected_hostname=$(echo $selected | awk -F' - ' '{print $2}')
+    my_hostname=$(hostname)
+    use_lan=$(echo -e "Yes\nNo" | rofi -dmenu -p "Use LAN IP for $selected_hostname?")
+    lan_command=""
+
+    if [ "$use_lan" == "Yes" ]; then
+        lan_command=" --exit-node-allow-lan-access=true"
+    fi
+
+    if [ "$selected" != "$selected_hostname" ]; then
+        # sudo tailscale up --exit-node $selected_ip$lan_command
+        echo "sudo tailscale up --exit-node $selected_ip$lan_command"
+    fi
+    exit
+fi
+
+if [ "$selected" == "Reset" ]; then
+    sudo tailscale up --reset
+    exit
+fi
+
+
 
