aae94b0aefd62515ede35b656ca9f84189b34496
Joshua Hegedus <josh.hegedus@outlook.com>
2024-05-09 15:15:28 +0200

    added easily changable keyboard

--- scripts/change-keyboard.sh
+++ scripts/change-keyboard.sh
@@ -2,23 +2,25 @@
 
 keyboard=$( cat $HOME/.keyboard )
 
+keyboard_count=5
+
 [ ! "$keyboard" -eq "$keyboard" ] 2>/dev/null && keyboard=0 
 
 [[ -z $keyboard ]] && keyboard=0
 
  (( keyboard+= 1 ))
 
-if (( $keyboard >= 4 )); then
+if (( $keyboard >= $keyboard_count )); then
 	keyboard=0
 fi
 
+declare -a keyboard_list
+keyboard_list[0]="xkb:us::eng"
+keyboard_list[1]="anthy"
+keyboard_list[2]="xkb:hu:101_qwerty_comma_nodead:hun"
+keyboard_list[3]="xkb:rs:latin:srp"
+keyboard_list[4]="xkb:rs::srp"
 
-case $keyboard in
-	0 )	ibus engine xkb:us::eng ;;
-	1 )	ibus engine anthy ;;
-	2 )	ibus engine xkb:hu:101_qwerty_comma_nodead:hun ;;
-	3 )	ibus engine xkb:rs:latin:srp ;;
-	* )	echo error > /tmp/lang.txt ;;
-esac
+ibus engine ${keyboard_list[$keyboard]}
 
 echo $keyboard > $HOME/.keyboard
