ad6ae7f0312cae7ccb707b613aa93748595c8937
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-10-18 16:02:58 +0200

    added rounded corners for picom

--- scripts/background.sh
+++ scripts/background.sh
@@ -1,35 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Exit if .env not exists
-[ -f "$HOME/.config/i3/.env" ] || exit 1
-
-# Load .env
-source "$HOME/.config/i3/.env"
-
-# Check if the ACCESS_KEY is set
-[ -z "$ACCESS_KEY" ] && echo "ACCESS_KEY is not set" && exit 1
-
-# Get a random image from the unsplash api use the $ACCESS_KEY
-
-url="https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}&collections=11445541&orientation=landscape"
-
-# Get the image fron a GET endpoint
-image_url=$(curl $url | jq -r '.urls.full')
 image_path="$HOME/.background.jpg"
 
-[ -z "$image_url" ] && echo "Image url is empty" && exit 1
+if [ -z $SKIP_BACKGROUND ]; then
+  echo "getting new image"
+  echo "testtest $SKIP_BACKGROUND" >$HOME/test-tmp.txt
+  exit 0
+  [ -f "$HOME/.config/i3/.env" ] || exit 1
+  source "$HOME/.config/i3/.env"
+  [ -z "$ACCESS_KEY" ] && echo "ACCESS_KEY is not set" && exit 1
+  url="https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}&collections=11445541&orientation=landscape"
+  image_url=$(curl $url | jq -r '.urls.full')
 
+  [ -z "$image_url" ] && echo "Image url is empty" && exit 1
+  curl -o $image_path $image_url
+else
+  echo "skipping background"
+fi
 
-# Download the image
-curl -o $image_path $image_url
-
-# Get the connected monitor names
-
+echo "using image $image_path"
 monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
-
-# Set the background image for each monitors
-
+echo "monitors: $monitors"
 for monitor in $monitors; do
-    feh --bg-fill --no-xinerama $image_path --bg-fill --no-xinerama $image_path --bg-fill --no-xinerama $image_path
+  feh --bg-fill --no-xinerama $image_path --bg-fill --no-xinerama $image_path --bg-fill --no-xinerama $image_path
 done
-
